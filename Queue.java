37490be13609d7d09f4a70fccad75da736e3e8fb
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2023-01-16 19:13:02 +0530

    Queue-Stack-DS

---
+++ Queue.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.day14.UC_Day14_Queue_Stack;
+
+public class Queue {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stack and Queue Data structure");
+
+}
+	
+}
