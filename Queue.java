d5d8947194d15ff055dae64a721eb2904028c863
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2023-01-16 19:15:13 +0530

    EnQueing Elements in Queue

--- Queue.java
+++ Queue.java
@@ -1,9 +1,22 @@
 package com.bridgelabz.day14.UC_Day14_Queue_Stack;
 
 public class Queue {
+	LinkedList list = new LinkedList();
+	public void enQueue(Object data)
+	{
+		list.addFirstNode(data);
+	}
+	public void display()
+	{
+		list.display();
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Stack and Queue Data structure");
-
-}
+		Queue queue = new Queue();
+		queue.enQueue(70);
+		queue.enQueue(30);
+		queue.enQueue(56);
+		queue.display();
+	}
 	
 }
